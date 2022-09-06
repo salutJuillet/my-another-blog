628a467efb1bcf1814134433ed6db693a7ce5592
salutJuillet <salut.juilllet@gmail.com>
2022-09-06 12:46:24 +0900

    form 완성

--- admin/src/components/CreatePost.jsx
+++ admin/src/components/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {ImSpinner11, ImEye, ImFilePicture, ImFileEmpty } from 'react-icons/im'
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
@@ -26,14 +26,90 @@ const MyStyle = styled.form`
   }
 `
 
-const mdRules = [
-  {titel: 'From h1 to h6', rule:'# Heading -> ###### Heading'},
-  {title: 'Blobkquote', rule: '> 인용문'},
-  {title: 'Image', rule: '![image alt](http://image_url.com)'},
-  {title: 'Link', rule: '[Link Text](http//your_link.com)'},
-]
+// const mdRules = [
+//   {titel: 'From h1 to h6', rule:'# Heading -> ###### Heading'},
+//   {title: 'Blobkquote', rule: '> 인용문'},
+//   {title: 'Image', rule: '![image alt](http://image_url.com)'},
+//   {title: 'Link', rule: '[Link Text](http//your_link.com)'},
+// ]
+
+const tagsArr = (text) => {
+  let arr = text.split(/[ ,]+/);
+  return arr;
+}
 
 const CreatePost = () => {
+  const [myForm, setMyForm] = useState({
+    title: '',
+    meta: '',
+    tags: [],
+    author: 'juillet',
+    slug: '',
+    file: null,
+    filename: ''
+  });
+
+  const [content, setContent] = useState('');
+  const [featured, setFeatured] = useState(false);
+  const handleFile = (e) => {
+    setMyForm({
+      ...myForm,
+      file: e.target.files[0],
+      filename: e.target.value
+    })
+    // console.dir(e.target.files);
+  }
+  const handleMyForm = (e) => {
+    setMyForm({
+      ...myForm,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleFeatured = () => {
+    setFeatured(!featured);
+  }
+
+  const enterComma = (e) => {
+    const {value} = e.target;
+    if(e.keyCode === 32){
+      setMyForm({
+        ...myForm,
+        tags: value + ','
+      })
+    }else{
+      return false;
+    }
+  }
+
+  const getContent = (content) => {
+    setContent(content.blocks[0].text);
+  }
+  
+  const onSubmit = (e) => {
+    // const url = '/post/create';
+    // const myform = new FormData();
+    // const arrTags = myForm.tags.split(', ');
+
+    // myform.append('title', myForm.title);
+    // myform.append('content', content);
+    // myform.append('author', myForm.author);
+    // myform.append('thumbnail', myForm.file);
+    // myform.append('tags', arrTags);
+    // myform.append('meta', myForm.meta);
+    // myform.append('slug', 'slug');
+    // const config = {
+    //   headers: {
+    //     'content-type': 'mulipart/form-data'
+    //   }
+    // }
+    // return post(url, myform, config);
+    e.preventDefault();
+    console.log(
+      myForm.title, content, myForm.file, myForm.tags, myForm.meta
+    )
+  }
+
   return (
     <MyStyle>
       <div className='flex items-center justify-between px-5'>
@@ -53,25 +129,42 @@ const CreatePost = () => {
       </div>
 
       <div className='px-5'> {/* featured checkbox */}
-        <input type='checkbox' id='featured' hidden />
+        <input type='checkbox' id='featured' hidden
+               defaultChecked={featured} 
+               onChange={handleFeatured} />
         <label htmlFor='featured' className='flex items-center space-x-2 cursor-pointer group'>
-          <div className='w-4 h-4 rounded-full border-2 border-gray-600 flex items-center justify-center group-hover:border-black'>
-            <div className='w-2 h-2 rounded-full bg-gray-600 group-hover:bg-black' />
+          <div className='w-4 h-4 rounded-full border-2 flex items-center justify-center border-black'>
+            {
+              featured ? (
+                <div className='w-2 h-2 rounded-full bg-black' />
+              ) : (
+                <div className='w-2 h-2 rounded-full bg-transparent' />
+              )
+            }
+            
           </div>
-          <span className='text-gray-600 group-hover:text-black hover:bg-transparent ring-black transition'>최근글로 등록</span>
+          <div className='hover:bg-transparent ring-black transition relative'>
+            최근글로 등록
+            <div className='hidden group-hover:block absolute bottom-0 bg-black' style={{width:88, height:2}} />
+          </div>
         </label>
       </div>
       
       <div className='px-5 bg-black rounded-full py-5 mt-2'>
           <ul className='mt-12'>
             <li> {/* title */}
-              <input type='text' name='title' placeholder='제목' className='border border-gray-800 rounded-md w-full my-1 p-1' id='title' />
+              <input type='text' name='title' placeholder='제목'
+                     className='border border-gray-800 rounded-md w-full my-1 p-1'
+                     id='title' 
+                     value={myForm.title}
+                     onChange={handleMyForm} />
             </li>
             <li> {/* content */}
               <Editor
                 toolbarClassName="toolbarClassName"
                 wrapperClassName="wrapper-class"
                 editorClassName="editor-class"
+                onChange={getContent}
               />
               {/* <textarea rows='20' name='content' className='border border-gray-800 rounded-md w-full mb-1 p-1 resize-none' id='content'></textarea> */}
             </li>
@@ -81,38 +174,36 @@ const CreatePost = () => {
                   <span>이미지 등록</span>
                   <ImFilePicture />
                 </label> */}
-                <input type='file' name='thumbnail' className='border border-gray-800 rounded-md w-full my-1 p-1 bg-white text-gray-400' id='image-input'  />
+                <input type='file' name='thumbnail' multiple={true}
+                       className='border border-gray-800 rounded-md w-full my-1 p-1 bg-white text-gray-400' 
+                       id='image-input'
+                       file={myForm.file}
+                       value={myForm.filename}
+                       onChange={handleFile}  />
               {/* </div>               */}
             </li>
             <li> {/* tags */}
-              <input type='text' name='tags' placeholder='태그' className='border border-gray-800 rounded-md w-full my-1 p-1' id='tags' />
+              <input type='text' name='tags' placeholder='태그' 
+                     className='border border-gray-800 rounded-md w-full my-1 p-1' 
+                     id='tags'
+                     value={myForm.tags}
+                     onChange={handleMyForm}
+                     onKeyDown={enterComma} />
             </li>
             <li> {/* meta description */}
-              <textarea rows='2' name='content' placeholder='간단한 설명' className='border border-gray-800 rounded-md w-full my-1 p-1 resize-none' id='meta'></textarea>
+              <textarea rows='2' name='meta' placeholder='간단한 설명' 
+                        className='border border-gray-800 rounded-md w-full my-1 p-1 resize-none' 
+                        id='meta'
+                        onChange={handleMyForm}
+                        value={myForm.meta} />
               {/* <input type='text' name='meta' placeholder='meta' className='border border-gray-800 rounded-md w-full my-1 p-1' id='meta description' /> */}
             </li>
 
           </ul>
-          
-{/* <div className='bg-white absolute top-1/2-translate-y-1/2 px-2 py-4 rounded'>
-  <h1 className='font-semibold text-center'>규칙</h1>
-  <ul className='space-y-2'>
-    {mdRules.map(({title, rule}) => {
-      return(
-        <li key={title}>
-          <p className='font-semibold text-gray-500'>{title}</p>
-          <p className='font-semibold text-gray-700 pl-2 font-mono'>{rule}</p>
-        </li>
-      )
-    })}
-    <li className='text-center'>
-      <a href="https://www.markdownguide.org/basic-syntax" target='_blank'>더보기</a>
-    </li>
-  </ul>
-</div> */}
 
           <div className='flex flex-row justify-center items-center my-7'>
-            <button className='border-2 rounded-full inline-block py-3 px-10 mx-2 hover:bg-white text-white bg-black hover:text-black'>등록</button>
+            <button className='border-2 rounded-full inline-block py-3 px-10 mx-2 hover:bg-white text-white bg-black hover:text-black'
+                    onClick={onSubmit}>등록</button>
             <button className='border-2 rounded-full inline-block py-3 px-10 mx-2 hover:bg-white text-white bg-black hover:text-black'>취소</button>
           </div>
       </div>
