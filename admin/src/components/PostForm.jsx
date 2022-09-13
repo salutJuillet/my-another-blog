a38c9bcf43a5588557c627f577fbb1ec0aaeca1d
salutJuillet <salut.juilllet@gmail.com>
2022-09-13 15:12:10 +0900

    컴포넌트 이름 변경

--- admin/src/components/PostForm.jsx
+++ admin/src/components/PostForm.jsx
@@ -1,8 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import {ImSpinner11, ImEye, ImFilePicture, ImFileEmpty } from 'react-icons/im'
+import { EditorState, convertToRaw } from 'draft-js'
 import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from 'draft-to-html';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import styled from 'styled-components'
+import post from '../api/post'
 
 const MyStyle = styled.form`
   .wrapper-class{
@@ -33,23 +36,17 @@ const MyStyle = styled.form`
 //   {title: 'Link', rule: '[Link Text](http//your_link.com)'},
 // ]
 
-const tagsArr = (text) => {
-  let arr = text.split(/[ ,]+/);
-  return arr;
-}
-
-const CreatePost = () => {
+const PostForm = () => {
   const [myForm, setMyForm] = useState({
     title: '',
     meta: '',
     tags: [],
     author: 'juillet',
-    slug: '',
     file: null,
     filename: ''
   });
 
-  const [content, setContent] = useState('');
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const [featured, setFeatured] = useState(false);
   const handleFile = (e) => {
     setMyForm({
@@ -59,6 +56,15 @@ const CreatePost = () => {
     })
     // console.dir(e.target.files);
   }
+
+  const titleRef = useRef();
+  const metaRef = useRef();
+  const tagRef = useRef();
+
+  // useEffect(()=>{
+  //   titleRef.current.focus(); //제목 input을 focused
+  // }, [])
+
   const handleMyForm = (e) => {
     setMyForm({
       ...myForm,
@@ -72,7 +78,7 @@ const CreatePost = () => {
 
   const enterComma = (e) => {
     const {value} = e.target;
-    if(e.keyCode === 32){
+    if(e.keyCode === 32){ //태그 작성 시 스페이스바를 눌렀을 때 자동으로 ,가 생성됨
       setMyForm({
         ...myForm,
         tags: value + ','
@@ -81,37 +87,43 @@ const CreatePost = () => {
       return false;
     }
   }
-
-  const getContent = (content) => {
-    setContent(content.blocks[0].text);
-  }
   
-  const onSubmit = (e) => {
-    // const url = '/post/create';
-    // const myform = new FormData();
-    // const arrTags = myForm.tags.split(', ');
-
-    // myform.append('title', myForm.title);
-    // myform.append('content', content);
-    // myform.append('author', myForm.author);
-    // myform.append('thumbnail', myForm.file);
-    // myform.append('tags', arrTags);
-    // myform.append('meta', myForm.meta);
-    // myform.append('slug', 'slug');
-    // const config = {
-    //   headers: {
-    //     'content-type': 'mulipart/form-data'
-    //   }
-    // }
-    // return post(url, myform, config);
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(
-      myForm.title, content, myForm.file, myForm.tags, myForm.meta
-    )
+    const { title, meta, tags, author } = myForm; //이 값들은 useState에서 들어온 값이므로 새로 FormData로 만들어서 보내줘야 한다. -> 105번째 줄
+    if(!title.trim()) return titleRef.current.focus();
+    if(!tags.trim()) return tagRef.current.focus();
+    if(!meta.trim()) return metaRef.current.focus();
+
+    const arrTags = myForm.tags.split(', ');
+    const slug = title.replace(/[^a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣 ]/g, ' ')
+                .split(" ")
+                .filter(item => item.trim())
+                .join('-'); //slug 자동 제작됨
+    const content = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('meta', meta);
+    formData.append('tags', arrTags);
+    formData.append('slug', slug);
+    formData.append('featured', featured);
+    formData.append('author', author);
+    const config = {
+      headers:{
+        'content-type':'multipart/form-data'
+      }
+    }
   }
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState)
+  }
+  // console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
 
   return (
-    <MyStyle>
+    <MyStyle onSubmit={handleFormSubmit}>
       <div className='flex items-center justify-between px-5'>
         <h1 className='text-xl font-semibold'>
           새 포스트 작성
@@ -145,7 +157,7 @@ const CreatePost = () => {
           </div>
           <div className='hover:bg-transparent ring-black transition relative'>
             최근글로 등록
-            <div className='hidden group-hover:block absolute bottom-0 bg-black' style={{width:88, height:2}} />
+            <div className='hidden group-hover:block absolute bottom-0.5 bg-black' style={{width:87.125, height:2}} />
           </div>
         </label>
       </div>
@@ -156,6 +168,7 @@ const CreatePost = () => {
               <input type='text' name='title' placeholder='제목'
                      className='border border-gray-800 rounded-md w-full my-1 p-1'
                      id='title' 
+                     ref={titleRef}
                      value={myForm.title}
                      onChange={handleMyForm} />
             </li>
@@ -164,7 +177,8 @@ const CreatePost = () => {
                 toolbarClassName="toolbarClassName"
                 wrapperClassName="wrapper-class"
                 editorClassName="editor-class"
-                onChange={getContent}
+                editorState={editorState}
+                onEditorStateChange={onEditorStateChange}
               />
               {/* <textarea rows='20' name='content' className='border border-gray-800 rounded-md w-full mb-1 p-1 resize-none' id='content'></textarea> */}
             </li>
@@ -186,6 +200,7 @@ const CreatePost = () => {
               <input type='text' name='tags' placeholder='태그' 
                      className='border border-gray-800 rounded-md w-full my-1 p-1' 
                      id='tags'
+                     ref={tagRef}
                      value={myForm.tags}
                      onChange={handleMyForm}
                      onKeyDown={enterComma} />
@@ -194,6 +209,7 @@ const CreatePost = () => {
               <textarea rows='2' name='meta' placeholder='간단한 설명' 
                         className='border border-gray-800 rounded-md w-full my-1 p-1 resize-none' 
                         id='meta'
+                        ref={metaRef}
                         onChange={handleMyForm}
                         value={myForm.meta} />
               {/* <input type='text' name='meta' placeholder='meta' className='border border-gray-800 rounded-md w-full my-1 p-1' id='meta description' /> */}
@@ -202,15 +218,12 @@ const CreatePost = () => {
           </ul>
 
           <div className='flex flex-row justify-center items-center my-7'>
-            <button className='border-2 rounded-full inline-block py-3 px-10 mx-2 hover:bg-white text-white bg-black hover:text-black'
-                    onClick={onSubmit}>등록</button>
+            <button type='submit' className='border-2 rounded-full inline-block py-3 px-10 mx-2 hover:bg-white text-white bg-black hover:text-black'>등록</button>
             <button className='border-2 rounded-full inline-block py-3 px-10 mx-2 hover:bg-white text-white bg-black hover:text-black'>취소</button>
           </div>
       </div>
-
     </MyStyle>
-    
   )
 }
 
-export default CreatePost
+export default PostForm
