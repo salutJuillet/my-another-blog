0a6658efc782bdf89f5d8ca0940bcb5b0f659e24
salutJuillet <salut.juilllet@gmail.com>
2022-09-21 10:17:45 +0900

    DeviceView.jsx 디자인 수정

--- admin/src/components/DeviceView.jsx
+++ admin/src/components/DeviceView.jsx
@@ -6,9 +6,9 @@ const DeviceView = ({onClose, thumbnail, title, content}) => {
     <div 
         onClick={onClose}
         className='bg-gray-500 bg-opacity-50 fixed inset-0 backdrop-blur-sm flex justify-center items-center'>
-        <div className='bg-white overflow-auto' style={{width:360, height:640}}>
+        <div className='bg-white overflow-auto border-2 border-dashed border-black rounded-md p-4' style={{width:360, height:640}}>
             <img src={thumbnail || undefined} alt={title || undefined} className='aspect-video object-contain' />
-            <div className='px-2'>
+            <div className='px-2 py-4'>
                 <h1 className="font-semibold text-gray-700 text-xl">{title || undefined}</h1>
                 <Markdown>{content}</Markdown>
             </div>
